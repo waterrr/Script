12ada564ed5aada720e2c35be0ba2d146f15ef91
waterrr <admin@gksec.com>
2020-09-14 01:05:40 +0800

    支持Ubuntu20.04换源

--- linux.sh
+++ linux.sh
@@ -18,6 +18,33 @@ check_sys(){
 	bit=`uname -m`
 }
 
+ubt2004(){
+check_sys
+[[ ${release} != "ubuntu" ]] || [[ $(lsb_release -r --short) != "20.04" ]] && echo -e "${Error} 本脚本不支持当前系统 ${release} $(lsb_release -r --short) !" && exit 1
+sudo echo "
+deb http://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse
+
+deb http://mirrors.aliyun.com/ubuntu/ focal-security main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-security main restricted universe multiverse
+
+deb http://mirrors.aliyun.com/ubuntu/ focal-updates main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-updates main restricted universe multiverse
+
+deb http://mirrors.aliyun.com/ubuntu/ focal-proposed main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-proposed main restricted universe multiverse
+
+deb http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse
+">/root/tmp
+sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
+sudo rm /etc/apt/sources.list
+sudo cp /root/tmp /etc/apt/sources.list
+sudo rm /root/tmp
+sudo echo "已经替换源为阿里源，默认源备份在【/etc/apt/sources.list.bak】"
+sudo echo "请尝试执行【apt update&apt upgrade】查看是否生效"
+}
+
 ubt1804(){
 check_sys
 [[ ${release} != "ubuntu" ]] || [[ $(lsb_release -r --short) != "18.04" ]] && echo -e "${Error} 本脚本不支持当前系统 ${release} $(lsb_release -r --short) !" && exit 1
@@ -110,13 +137,14 @@ echo -e "  Linux服务器常见一键脚本 ${Red_font_prefix}[0.1]${Font_color_
   ---- W4ter | https://github.com/waterrr  ----
   ---- iriszero | https://github.com/iriszero48  ----
 
-  ${Green_font_prefix}1.${Font_color_suffix} 更换Ubuntu18.04(bionic)默认源为阿里源
-  ${Green_font_prefix}2.${Font_color_suffix} 更换Ubuntu16.04默认源为阿里源
-  ${Green_font_prefix}3.${Font_color_suffix} 更换PyPI默认源为阿里镜像源
+  ${Green_font_prefix}1.${Font_color_suffix} 更换Ubuntu20.04(focal)默认源为阿里源
+  ${Green_font_prefix}2.${Font_color_suffix} 更换Ubuntu18.04(bionic)默认源为阿里源
+  ${Green_font_prefix}3.${Font_color_suffix} 更换Ubuntu16.04默认源为阿里源
+  ${Green_font_prefix}4.${Font_color_suffix} 更换PyPI默认源为阿里镜像源
 ————————————
-  ${Green_font_prefix}4.${Font_color_suffix} 一键安装Docker【国内镜像】
-  ${Green_font_prefix}5.${Font_color_suffix} 一键安装Docker Compose【国内镜像】
-  ${Green_font_prefix}6.${Font_color_suffix} 一键加速DockerHub镜像【Azure国内镜像】
+  ${Green_font_prefix}5.${Font_color_suffix} 一键安装Docker【国内镜像】
+  ${Green_font_prefix}6.${Font_color_suffix} 一键安装Docker Compose【国内镜像】
+  ${Green_font_prefix}7.${Font_color_suffix} 一键加速DockerHub镜像【Azure国内镜像】
 ————————————
   ${Green_font_prefix}0.${Font_color_suffix} ${Red_font_prefix}EXIT${Font_color_suffix}
  "
@@ -126,21 +154,24 @@ case "$num" in
 	exit 0
 	;;
 	1)
-	ubt1804
+	ubt2004
 	;;
 	2)
-	ubt1604
+	ubt1804
 	;;
 	3)
-	PyPI
+	ubt1604
 	;;
 	4)
-	Docker
+	PyPI
 	;;
 	5)
-	Docker_Compose
+	Docker
 	;;
 	6)
+	Docker_Compose
+	;;
+	7)
 	Docker_Hub
 	;;
 	*)
