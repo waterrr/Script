f5c15d60156c64225506069c6ef4c3414f2b1395
W4ter <admin@gksec.com>
2021-09-06 16:35:46 +0800

    [+]增加了ARM换源支持

--- linux.sh
+++ linux.sh
@@ -20,6 +20,9 @@ check_sys(){
 
 ubt2004(){
 check_sys
+[[ ${release} = "ubuntu" ]] && [[ $(lsb_release -r --short) = "20.04" ]] && [[ $(uname -m) = "aarch64" ]] && sudo cp -a /etc/apt/sources.list /etc/apt/sources.list.bak && sudo wget -O /etc/apt/sources.list https://cdn.jsdelivr.net/gh/waterrr/Script/arm/Ubuntu-Ports-focal.list && echo "已经替换源为阿里源(ARM)，默认源备份在【/etc/apt/sources.list.bak】"&& exit 1
+
+check_sys
 [[ ${release} != "ubuntu" ]] || [[ $(lsb_release -r --short) != "20.04" ]] && echo -e "${Error} 本脚本不支持当前系统 ${release} $(lsb_release -r --short) !" && exit 1
 sudo echo "
 deb http://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse
@@ -47,6 +50,9 @@ sudo echo "请尝试执行【apt update&apt upgrade】查看是否生效"
 
 ubt1804(){
 check_sys
+[[ ${release} = "ubuntu" ]] && [[ $(lsb_release -r --short) = "18.04" ]] && [[ $(uname -m) = "aarch64" ]] && sudo cp -a /etc/apt/sources.list /etc/apt/sources.list.bak && sudo wget -O /etc/apt/sources.list https://cdn.jsdelivr.net/gh/waterrr/Script/arm/Ubuntu-Ports-bionic.list && echo "已经替换源为阿里源(ARM)，默认源备份在【/etc/apt/sources.list.bak】"&& exit 1
+
+check_sys
 [[ ${release} != "ubuntu" ]] || [[ $(lsb_release -r --short) != "18.04" ]] && echo -e "${Error} 本脚本不支持当前系统 ${release} $(lsb_release -r --short) !" && exit 1
 sudo echo "
 deb http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
@@ -73,6 +79,9 @@ sudo echo "请尝试执行【apt update&apt upgrade】查看是否生效"
 }
 
 ubt1604(){
+check_sys
+[[ ${release} = "ubuntu" ]] && [[ $(lsb_release -r --short) = "16.04" ]] && [[ $(uname -m) = "aarch64" ]] && sudo cp -a /etc/apt/sources.list /etc/apt/sources.list.bak && sudo wget -O /etc/apt/sources.list https://cdn.jsdelivr.net/gh/waterrr/Script/arm/Ubuntu-Ports-xenial.list && echo "已经替换源为阿里源(ARM)，默认源备份在【/etc/apt/sources.list.bak】"&& exit 1
+
 check_sys
 [[ ${release} != "ubuntu" ]] || [[ $(lsb_release -r --short) != "16.04" ]] && echo -e "${Error} 本脚本不支持当前系统 ${release} $(lsb_release -r --short) !" && exit 1
 sudo echo "
@@ -148,38 +157,38 @@ sudo echo "已经替换DockerHub为国内阿里/中科大源"
 }
 
 Superbench(){
-    while [ ! -f ./superbench2.sh ]; do
-            wget -N -P ./ --no-check-certificate https://cdn.jsdelivr.net/gh/waterrr/Script/superbench2.sh
+    while [ ! -f ./bench ]; do
+            wget https://bench.im/bench.py -O bench
     done
-    sudo bash ./superbench2.sh
-    sudo rm ./superbench2.sh
+    sudo python3 bench
+    sudo rm ./bench
     break
 }
 
 Netflix_check(){
-	while [ ! -f ./superbench2.sh ]; do
-        wget -N -P ./ --no-check-certificate https://cdn.jsdelivr.net/gh/waterrr/Script/netflix_check.sh
+	while [ ! -f ./check.sh ]; do
+        wget -N -P ./ --no-check-certificate https://cdn.jsdelivr.net/gh/waterrr/Script/check.sh
     done
 chmod +x netflix_check.sh
 bash netflix_check.sh
 }
 
-echo -e "  Linux服务器常见一键脚本 ${Red_font_prefix}[0.2]${Font_color_suffix}
-  ----Upgrade in 2021-02-25 ----
+echo -e "  Linux服务器常见一键脚本 ${Red_font_prefix}[0.3]${Font_color_suffix}
+  ----Upgrade in 2021-09-06 ----
   ---- W4ter | https://github.com/waterrr  ----
   ---- iriszero | https://github.com/iriszero48  ----
 
-  ${Green_font_prefix}1.${Font_color_suffix} 更换Ubuntu20.04(focal)默认源为阿里源
-  ${Green_font_prefix}2.${Font_color_suffix} 更换Ubuntu18.04(bionic)默认源为阿里源
-  ${Green_font_prefix}3.${Font_color_suffix} 更换Ubuntu16.04默认源为阿里源
+  ${Green_font_prefix}1.${Font_color_suffix} 更换Ubuntu20.04(focal)默认源为阿里源【支持ARM】
+  ${Green_font_prefix}2.${Font_color_suffix} 更换Ubuntu18.04(bionic)默认源为阿里源【支持ARM】
+  ${Green_font_prefix}3.${Font_color_suffix} 更换Ubuntu16.04(xenial)默认源为阿里源【支持ARM】
   ${Green_font_prefix}4.${Font_color_suffix} 更换PyPI默认源为阿里镜像源
 ————————————
   ${Green_font_prefix}5.${Font_color_suffix} 一键安装Docker【国内镜像】
   ${Green_font_prefix}6.${Font_color_suffix} 一键安装Docker Compose【国内镜像】
   ${Green_font_prefix}7.${Font_color_suffix} 一键加速DockerHub镜像【阿里/中科大源】
 ————————————
-  ${Green_font_prefix}8.${Font_color_suffix} 一键测试服务器性能/网速 | Superbench修复版
-  ${Green_font_prefix}9.${Font_color_suffix} 一键测试服务器是否能看Netflix和解锁区域
+  ${Green_font_prefix}8.${Font_color_suffix} 一键测试网速 | bench.im【需要python3支持】
+  ${Green_font_prefix}9.${Font_color_suffix} 一键测试服务器流媒体解锁
 ————————————
   ${Green_font_prefix}0.${Font_color_suffix} ${Red_font_prefix}EXIT${Font_color_suffix}
  "
